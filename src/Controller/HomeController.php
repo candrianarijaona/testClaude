e0304ccddbf1749682b47bb772851308549f0e81
Claude Andrianarijaona <samuelson.claude@gmail.com>
2023-06-23 18:25:47 +0200

    adding weather data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(HttpClientInterface $client): Response
     {
-        return $this->render('home/index.html.twig');
+        $url = sprintf('https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s&lang=%s&units=%s',
+            '48.873487614726734',
+            '2.3303383259931723',
+            'acc2caec8e1294a40424dde232848027',
+            'fr',
+            'Metric'
+        );
+
+        $response = $client->request('GET', $url);
+
+        return $this->render('home/index.html.twig',
+        [
+            'data' => json_decode($response->getContent(), true),
+        ]);
     }
 }
